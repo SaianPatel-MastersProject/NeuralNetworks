3974767b045888d97b86d96c8259978f5dd25153
Saian Patel <saianp@icloud.com>
2025-01-28 14:19:51 +0000

    SM34

---
+++ FFNN/Iteration34/PlotInputSpaceOverlay.m
@@ -0,0 +1,12 @@
+%% Script to overlay a run with the input space
+
+% Load Run Struct
+load('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP01_27\2025_FYP01_27_D2_R05.mat')
+
+% Load layers
+runStruct = Utilities.fnLoadLayer(runStruct, 'PE');
+runStruct = Utilities.fnLoadLayer(runStruct, 'KAP');
+
+%%
+% Call the plotter
+fnInputSpaceOverlay(trainingData, runStruct.data(and(runStruct.data.lapNumber > 0, runStruct.data.lapNumber < 12), :), [1:4]);
