c70b3a248c99fc86ca9b5e747755e94ef976798e
Saian Patel <saianp@icloud.com>
2025-02-09 10:51:51 +0000

    SM42 Plots

--- FFNN/Iteration42/plottingScript.m
+++ FFNN/Iteration42/plottingScript.m
@@ -4,25 +4,33 @@ obj = Plotting.multiPlotter();
 
 % Add training run
 obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D1_R02.mat', true, [2:31]);
-obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D1_R03.mat', true, [2:4]);
-obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D2_R02.mat', true, [2:4]);
+obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D7_R02.mat', true, [1,2]);
+
 
 
 % Add reference lap
 obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D1_R02.mat', 31); % Human
-obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D1_R03.mat', 4); % FFNN 39
-obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D2_R02.mat', 4); % FFNN 40
+obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D7_R02.mat', 2); % FFNN 42
 
 %%%%%%%% Plotting Commands
 
+%% Set plot colours
+% Human as blue
+% FFNN as red
+obj = obj.addLapsColours({'#0077FF', '#FF0000'});
+
 %% Plot the racing line
 obj.plotRacingLine(true);
+obj.plotLineDistributionPerCorner(1, true, [1,2]);
 
 %% Plot steering angle with errors
 obj.plotErrorsWithSteering();
 
 %% Plot steering and its derivatives
 obj.plotDerivativesSteeringAngle();
+
+%% Plot PSPectrum
+obj.plotPSpectrum('steerAngle', 'Run')
 
 %% Plot variability relative to the human data
 obj.plotEnvelope(1, 'CTE', true);
@@ -34,9 +42,10 @@ obj.plotEnvelope(2, 'CTE', false);
 obj.plotEnvelope(2, 'HeadingError', false);
 obj.plotEnvelope(2, 'steerAngle', false);
 
-%% Plot PSPectrum
-obj.plotPSpectrum('steerAngle', 'Run')
-
 %% Plot Violins
 obj.plotRunViolins();
 obj.plotGroupedMetrics();
+
+%% Plot TF Estimate
+obj.plotTF('CTE', 'steerAngle', 'Lap');
+obj.plotTF('kappa', 'steerAngle', 'Lap');
