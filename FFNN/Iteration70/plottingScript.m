039da128ef422be79394aab9a3fb01ceb46e4990
Saian Patel <saianp@icloud.com>
2025-03-20 12:18:39 +0000

    SM70

---
+++ FFNN/Iteration70/plottingScript.m
@@ -0,0 +1,61 @@
+%% Plotting Script - Iteration 54
+
+obj = Plotting.multiPlotter();
+
+% Add training run
+obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D1_R02.mat', true, [2:31]);
+obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP03_17\2025_FYP03_17_D4_R01.mat', true, [2:4]);
+obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP03_17\2025_FYP03_17_D4_R02.mat', true, [2:4]);
+obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_10\2025_FYP02_10_D1_R02.mat', true, [2:4]);
+
+
+% Add reference lap
+obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_03\2025_FYP02_03_D1_R02.mat', 31); % Human
+obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP03_17\2025_FYP03_17_D4_R01.mat', 4); % FFNN 70
+obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP03_17\2025_FYP03_17_D4_R02.mat', 4); % FFNN 70 - Gain 2
+obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP02_10\2025_FYP02_10_D1_R02.mat', 4); % FFNN 48
+
+%%%%%%%% Plotting Commands
+%% Set plot colours
+% Human as blue
+% FFNN as red
+obj = obj.addLapsColours({'#0077FF', '#FF0000', '#00FF00'});
+%% Plot the racing line
+obj.plotRacingLine(true);
+obj.plotLineDistributionPerCorner(1, true, [1,2])
+
+%% Plot steering angle with errors
+obj.plotErrorsWithSteering();
+
+%% Plot steering and its derivatives
+obj.plotDerivativesSteeringAngle();
+
+%% Plot variability relative to the human data
+obj.plotEnvelope(1, 'CTE', true);
+obj.plotEnvelope(1, 'HeadingError', true);
+obj.plotEnvelope(1, 'steerAngle', true);
+
+%% Plot variability relative of the FFNN
+obj.plotEnvelope(2, 'CTE', false);
+obj.plotEnvelope(2, 'HeadingError', false);
+obj.plotEnvelope(2, 'steerAngle', false);
+
+%% Plot PSPectrum
+obj.plotPSpectrum('steerAngle', 'Run')
+obj.plotPSpectrum('CTE', 'Run')
+
+%% Plot Violins
+obj.plotRunViolins();
+obj.plotGroupedMetrics();
+
+%% Plot TF Estimate
+obj.plotTF('CTE', 'steerAngle', 'Lap', false);
+obj.plotTF('kappa', 'steerAngle', 'Lap', false);
+
+%% Create averages tables
+avgMetricsCTE(1,:) = mean(obj.runData(1).metricsCTE,1);
+avgMetricsCTE(2,:) = mean(obj.runData(2).metricsCTE,1);
+avgMetricsCTE(3,:) = mean(obj.runData(3).metricsCTE,1);
+
+avgMetricsSteer(1,:) = mean(obj.runData(1).metricsSteer,1);
+avgMetricsSteer(2,:) = mean(obj.runData(2).metricsSteer,1);
