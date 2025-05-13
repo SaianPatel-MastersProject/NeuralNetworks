8ad2fadb60bf0158fb4fc107cec39a1826a86866
Saian Patel <saianp@icloud.com>
2025-05-13 13:38:38 +0100

    SM127

---
+++ FFNN/Iteration127/plottingScript.m
@@ -0,0 +1,60 @@
+%% Load
+obj = Plotting.multiPlotter();
+
+% Add training run
+obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP04_14\2025_FYP04_14_D7_R03.mat', true, [2:10]); % SP k=-1.5
+obj = obj.addRun('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP05_05\2025_FYP05_05_D1_R03.mat', true, [2:4]); % SM108
+
+
+
+% Add reference lap
+obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP04_14\2025_FYP04_14_D7_R03.mat', 10); % SP
+obj = obj.addLap('D:\Users\Saian\Workspace\Data\+ProcessedData\2025\FYP05_05\2025_FYP05_05_D1_R03.mat', 4); % SM108
+
+% Overwrite the legend cell
+obj.plottingTools.legendCell = {'SP', 'SM108'};
+
+%% Stats Test & Metrics
+metricsComp = obj.fnMetricsComparison(1, [2]);
+CI = obj.fnConfidenceIntervals(1, 2, 0.05);
+
+%%%%%%%% Plotting Commands
+%% Set plot colours
+% Human as blue
+% FFNN as red
+obj = obj.addLapsColours({'#0077FF', '#FF0000'});
+%% Plot the racing line
+obj.plotRacingLine(true);
+obj.plotLineDistribution(1, true, [2])
+%% Plot steering angle with errors
+obj.plotErrorsWithSteering();
+
+%% Plot steering and its derivatives
+obj.plotDerivativesSteeringAngle(true);
+
+%% Plot variability relative to the human data
+obj.plotEnvelope(1, 'CTE', true);
+obj.plotEnvelope(1, 'HeadingError', true);
+obj.plotEnvelope(1, 'steerAngle', true);
+
+%% Plot variability relative of the FFNN
+obj.plotEnvelope(2, 'CTE', false);
+obj.plotEnvelope(2, 'HeadingError', false);
+obj.plotEnvelope(2, 'steerAngle', false);
+
+%% Plot PSPectrum
+obj.plotPSpectrum('steerAngle', 'Run')
+xlim([0, 5])
+obj.plotPSpectrum('dSteerAngle', 'Run')
+xlim([0, 5])
+obj.plotPSpectrum('CTE', 'Run')
+xlim([0, 5])
+
+%% Plot Violins
+obj.plotRunViolins();
+obj.plotGroupedMetrics();
+
+%% Plot TF Estimate
+obj.plotTF('CTE', 'steerAngle', 'Lap', false);
+obj.plotTF('kappa', 'steerAngle', 'Lap', false);
+
